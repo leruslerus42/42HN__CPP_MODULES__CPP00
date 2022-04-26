bf310d29b8d78f61a9f37e633610269c9394ac8b
leruslerus42 <ruslan.rajaobelina@gmail.com>
2022-04-26 09:59:36 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,18 +6,39 @@
 /*   By: rrajaobe <rrajaobe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 10:42:19 by rrajaobe          #+#    #+#             */
-/*   Updated: 2022/04/25 10:54:13 by rrajaobe         ###   ########.fr       */
+/*   Updated: 2022/04/26 09:45:03 by rrajaobe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
 
+using namespace std;
 
-
-int main()
+int main(int argc, char **argv)
 {
+	int i = 1;
+	int j = 0;
+
+	if ( argc == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+
+	while (i < argc)
+	{
+		while (argv[i][j])
+		{
+			if (argv[i][j] > 96 && argv[i][j] < 123)
+				argv[i][j] -= 32;
+			if (argv[i][j] != '"')
+				cout << argv[i][j];
+			j++;
+		}
+		if (i != argc - 1 || argv[i][j-1] != '"')
+			cout << ' ';
+		j = 0;
+		i++;
+	}
 
 
+	
 	return (0);
 }
